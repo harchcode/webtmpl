b935bfcf737a4b1cae9b7e35cfab174410d76cde
Hartono Chandra <harchcode@gmail.com>
2021-04-23 16:51:25 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+## Prerequisite
+* Node.js
+
+## Installation
+* Clone this repo.
+* Go to directory.
+* `npm install`
+
+## Development
+* `npm start`
+
+## Update npm packages to latest
+`npx npm-check-updates -u+`
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.css", "./src/**/*.ts"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
